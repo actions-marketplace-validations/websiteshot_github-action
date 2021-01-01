856ffd4c0f4f4089273675492225d27d1b475f10
Adam <i@adamurban.de>
2021-01-01 09:12:43 +0100

    fix github core import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import core from '@actions/core'
 import { Runner } from './controller/runner.controller'
 import { Validation } from './controller/validation.controller'
+const core = require('@actions/core')
 
 async function run() {
   try {
